ef54e8c6202c8ccbba06cd551f929a4a13b96aae
saraen7 <sara.e.norlin@outlook.com>
2017-10-23 10:02:49 -0400

    colorSwitcher
    Color Switcher complete

---
+++ js/index.js
@@ -0,0 +1,31 @@
+/*Before refactoring
+$('#grayButton').click(switchGray);
+$('#whiteButton').click(switchWhite);
+$('#blueButton').click(switchBlue);
+$('#yellowButton').click(switchYellow);
+
+
+function switchGray() {
+  $('body').attr('class', 'gray');
+}
+
+function switchWhite() {
+  $('body').attr('class', 'white');
+}
+
+function switchBlue() {
+  $('body').attr('class', 'blue');
+}
+
+function switchYellow() {
+  $('body').attr('class', 'yellow');
+}*/
+
+function changeColor() {
+  var newColor = $(this).attr("class");
+  $("body").attr("class", newColor);
+}
+
+$(document).ready(function(){
+  $("#switcher li").on("click", changeColor);
+});
